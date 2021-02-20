0f9bd5c3387eacdc5e6f8b7f23bfe0838740adf7
Sean Riedl <smriedl@gmail.com>
2021-02-20 14:00:02 -0500

    Generate comments scaffold and add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
  
   has_many :posts, dependent: :destroy
   has_many :friend_posts, through: :friends, source: :posts
+  has_many :comments, dependent: :destroy
 
   has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id', inverse_of: 'sent_by', dependent: :destroy
   has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id', inverse_of: 'sent_to', dependent: :destroy
