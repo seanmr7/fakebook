e5e30b1a8d9be86110a0728df009651f66c3b791
Sean Riedl <smriedl@gmail.com>
2021-02-24 20:33:45 -0500

    Add friend notification to navbar

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,7 +7,7 @@ class FriendshipsController < ApplicationController
     if @friendship.save
 
       # Create notifications for sent_to user
-      Notification.create(recipient: params[:user_id], actor: current_user, action: "#{current_user.full_name} wants to be friends", notifiable: @friendship)
+      Notification.create(recipient: User.find(params[:user_id]), actor: current_user, action: "#{current_user.full_name} wants to be friends", notifiable: @friendship)
 
       flash[:success] = "New request sent!"
     else
