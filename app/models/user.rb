a136fb5da8150d83576ddc8f786981d74d6d0553
Sean Riedl <smriedl@gmail.com>
2021-02-27 17:23:50 -0500

    Finish Omniauth in development environment

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     name_split = auth.info.name.split(" ")
     user = User.where(email: auth.info.email).first
-    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[0], first_name: name_split[1], email: auth.info.email, password: Devise.friendly_token[0, 20])
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, last_name: name_split[1], first_name: name_split[0], email: auth.info.email, password: Devise.friendly_token[0, 20])
     user
   end
   
